bb8d17a5a1978a723a873337c3c9e07e8357ad32
Raphael Rodolfo <gomesrodolfo656@gmail.com>
2025-08-24 19:32:57 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const nome = "Raphael Rodolfo";
+
+const idade = 2025 - 2003;
+
+const str = "Olá eu sou";
+
+const strTemplateString = `${str} ${nome}.
+E minha idade é ${idade}`;
+
+console.log(strTemplateString);
